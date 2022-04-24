5b1ef28bf3f7e89d2f870be5694b0ffca6bd711a
madlur <2185430@gmail.com>
2022-04-24 10:15:44 +0300

    2. Описать ошибки в коде и предложить варианты оптимизации:

---
+++ lesson01/src/main/java/codereview/LightWeightCar.java
@@ -0,0 +1,17 @@
+package codereview;
+
+class LightWeightCar extends Car implements Moveable, Stopable { //добавить Stopable и переопределить метод
+    @Override
+    void open() {
+        System.out.println("Car is open");
+    }
+    @Override
+    public void move() {
+        System.out.println("Car is moving");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Car is stopping");
+    }
+}
---
+++ lesson01/src/main/java/codereview/Moveable.java
@@ -0,0 +1,5 @@
+package codereview;
+
+public interface Moveable {
+    void move();
+}
